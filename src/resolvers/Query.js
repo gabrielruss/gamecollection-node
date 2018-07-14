33c50fcc03d1d2bee3eae60f39d9c2d4e3e30db3
Gabriel <gabrielruss88@gmail.com>
2018-07-13 20:28:47 -0400

    added a inventory filter

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,16 @@
 const info = () => `This is my game collection API`;
 
 function inventory(parent, args, context, info) {
-  return context.db.query.games({}, info);
+  const where = args.filter
+    ? {
+        OR: [
+          { name_contains: args.filter },
+          { gameConsole_contains: args.filter },
+          { condition_contains: args.filter }
+        ]
+      }
+    : {};
+  return context.db.query.games({ where }, info);
 }
 
 module.exports = {
